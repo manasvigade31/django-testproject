c0f1c69f57e97692329630ee7907541b3a094669
manasvigade31 <kalyanigadenagpur@gmail.com>
2024-06-26 19:12:03 +0530

    Update views.py

--- my_app/views.py
+++ my_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello, Manasvi You are Doing Great Keep It Up!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!:)')
+    return HttpResponse('Hello, Sahil!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!:)')
 
